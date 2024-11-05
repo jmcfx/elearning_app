440c158d3866bf9f1df516a1b8fd4b26c3dc6a29
jmcfx <uispoj@gmail.com>
2024-11-05 15:58:23 +0100

    onBoarding Screen

---
+++ lib/pages/welcome/bloc/welcome_blocs.dart
@@ -0,0 +1,12 @@
+import 'package:elearning_app/pages/welcome/bloc/welcome_events.dart';
+import 'package:elearning_app/pages/welcome/bloc/welcome_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class WelcomeBlocs extends Bloc<WelcomeEvents, WelcomeState> {
+  WelcomeBlocs() : super(WelcomeState()) {
+    on<WelcomeEvents>((event, emit) {
+      emit(WelcomeState(page:state.page));
+    });
+  
+  }
+}
